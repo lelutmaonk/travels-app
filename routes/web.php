0ec331f2b63189290af86205b8c123e8e44ea695
lelutmaonk <putuadiputra51@gmail.com>
2023-07-21 00:29:14 +0800

    add templates admin

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.layouts.index');
+    return view('admin.index', [
+        'title' => 'Test'
+    ]);
 });
